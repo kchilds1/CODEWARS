310bdd0c73bc5e09ef98dd8861ba1b426a2aa423
kchilds1 <kenneth.childs20@gmail.com>
2024-01-26 19:31:00 -0600

    repeating

---
+++ repeatStr/index.js
@@ -0,0 +1,9 @@
+function repeatStr (n, s) {
+
+    console.log(s.repeat(n))
+    //return '';
+  }
+  
+  //s needs to be repeated n times so I will need to use the repeat method
+
+  repeatStr(2, " Hello");
